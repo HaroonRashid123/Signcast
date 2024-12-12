89709847f84fb5918053ba9ed2c7c50dce1e5f7f
HaroonRashid123 <haroonrashid1072@gmail.com>
2024-12-12 14:18:38 -0500

    take off old client

--- public/client.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const newProjectBtn = document.getElementById('newProject');
-    
-    if (newProjectBtn) {
-        newProjectBtn.addEventListener('click', function() {
-            window.location.href = '/planner';
-        });
-    }
-}); 
